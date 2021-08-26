c7d1467d1848363eb0fd6c73292462771be4413f
van-khoe <khoelatui@gmail.com>
2021-08-26 18:02:18 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trước khi học lập trình, anh là người hướng nội. Sau khi học lập trình anh là người hướng đối tượng. Và sau khi quen em, anh chuyển sang hướng về em.`,
+    btnIntro: 'Tiếp tục >>',
+    title: 'Lời cảm ơn vì bao ngày qua, cậu luôn ở bên tôi ko rời xa',
+    desc: 'Vậy thì tớ yêu cậu mất thôi ôi giồi ôi, phải làm sao để thôi nhớ nhung ôi tôi yêu mất rồi 😙',
+    btnYes: 'Thích anh mất rùiii <3',
+    btnNo: 'Anh ảo tưởng à :3',
+    question:'Em đã nhìn trúng anh ở điểm nào ròi 😙',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Nếu em muốn nói gì với anh thì nói ngay đi nhớ :3.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100011758017742' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
